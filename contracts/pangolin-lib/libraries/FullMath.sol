b93835964052cb656ea632f3aad8756ae6b7a133
Connor Daly <connor.daly@avalabs.org>
2021-01-03 22:22:58 -0500

    Added Hardhat
    Added hardhat files to help deploy and test.

--- contracts/pangolin-lib/libraries/FullMath.sol
+++ contracts/pangolin-lib/libraries/FullMath.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CC-BY-4.0
-pragma solidity >=0.4.0;
+pragma solidity >=0.4.0 <0.8.0;
 
 // taken from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
 // license is CC-BY-4.0
