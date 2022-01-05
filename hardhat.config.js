20c20842a47336250bc1e364d754ba47ea0f20f8
Sunur Efe Vural <efe@efe.kim>
2022-01-05 18:28:06 -0500

    duplicate libraries & add dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,6 +18,9 @@ module.exports = {
   solidity: {
     compilers: [
       {
+        version: "0.4.15"
+      },
+      {
         version: "0.5.16"
       },
       {
@@ -33,7 +36,19 @@ module.exports = {
         }
       },
       {
+        version: "0.6.12"
+      },
+      {
         version: "0.7.0"
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000
+          }
+        }
       },
       {
         version: "0.8.0"
@@ -122,7 +137,7 @@ module.exports = {
       url: 'https://rpc.xdaichain.com/',
       gasPrice: 470000000000,
       chainId: 100,
-      accounts: [] 
+      accounts: []
     }
   }
 };
