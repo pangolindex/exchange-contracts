a9109e1a0a3a884342199e5460e8570bc8938a4c
Sunur Efe Vural <efe@efe.kim>
2022-01-06 17:02:54 -0500

    update minichef to latest version in governance

--- contracts/dex/MiniChefV2.sol
+++ contracts/dex/MiniChefV2.sol
@@ -4,7 +4,6 @@ pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
-import "@boringcrypto/boring-solidity/contracts/BoringBatchable.sol";
 import "openzeppelin-contracts-legacy/access/Ownable.sol";
 import "./libraries/SignedSafeMath.sol";
 import "./interfaces/IRewarder.sol";
@@ -15,11 +14,9 @@ interface IMigratorChef {
     function migrate(IERC20 token) external returns (IERC20);
 }
 
-/// @notice The (older) MasterChef contract gives out a constant number of SUSHI tokens per block.
-/// It is the only address with minting rights for SUSHI.
-/// The idea for this MasterChef V2 (MCV2) contract is therefore to be the owner of a dummy token
-/// that is deposited into the MasterChef V1 (MCV1) contract.
-/// The allocation point for this pool on MCV1 is the total allocation point for all pools that receive double incentives.
+/// @notice The (older) MiniChefV2 contract gives out a reward tokens per block.
+// The reward tokens must be topped up regularly to ensure users do not accrue non-existent rewards
+// This implementation aims to avoid that issue by using a dynamic window of funding
 contract MiniChefV2 is Ownable {
     using BoringMath for uint256;
     using BoringMath128 for uint128;
@@ -28,7 +25,7 @@ contract MiniChefV2 is Ownable {
 
     /// @notice Info of each MCV2 user.
     /// `amount` LP token amount the user has provided.
-    /// `rewardDebt` The amount of SUSHI entitled to the user.
+    /// `rewardDebt` The amount of reward entitled to the user.
     struct UserInfo {
         uint256 amount;
         int256 rewardDebt;
@@ -36,15 +33,15 @@ contract MiniChefV2 is Ownable {
 
     /// @notice Info of each MCV2 pool.
     /// `allocPoint` The amount of allocation points assigned to the pool.
-    /// Also known as the amount of SUSHI to distribute per block.
+    /// Also known as the amount of reward to distribute per block.
     struct PoolInfo {
-        uint128 accSushiPerShare;
+        uint128 accRewardPerShare;
         uint64 lastRewardTime;
         uint64 allocPoint;
     }
 
-    /// @notice Address of SUSHI contract.
-    IERC20 public immutable SUSHI;
+    /// @notice Address of reward (PNG) contract.
+    IERC20 public immutable REWARD;
     // @notice The migrator contract. It has a lot of power. Can only be set through governance (owner).
     IMigratorChef public migrator;
     bool public migrationDisabled;
@@ -68,11 +65,11 @@ contract MiniChefV2 is Ownable {
     /// @dev Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint;
 
-    uint256 public sushiPerSecond;
-    uint256 private constant ACC_SUSHI_PRECISION = 1e12;
+    uint256 public rewardPerSecond;
+    uint256 private constant ACC_REWARD_PRECISION = 1e12;
 
     /// @dev Block time when the rewards per second stops
-    uint256 rewardsExpiration;
+    uint256 public rewardsExpiration;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
@@ -80,30 +77,41 @@ contract MiniChefV2 is Ownable {
     event Harvest(address indexed user, uint256 indexed pid, uint256 amount);
     event PoolAdded(uint256 indexed pid, uint256 allocPoint, IERC20 indexed lpToken, IRewarder indexed rewarder);
     event PoolSet(uint256 indexed pid, uint256 allocPoint, IRewarder indexed rewarder, bool overwrite);
-    event PoolUpdate(uint256 indexed pid, uint64 lastRewardTime, uint256 lpSupply, uint256 accSushiPerShare);
+    event PoolUpdate(uint256 indexed pid, uint64 lastRewardTime, uint256 lpSupply, uint256 accRewardPerShare);
     event MigratorSet(address migrator);
     event MigratorDisabled();
     event Migrate(uint256 pid);
     event FunderAdded(address funder);
     event FunderRemoved(address funder);
-    event LogSushiPerSecond(uint256 sushiPerSecond);
+    event LogRewardPerSecond(uint256 rewardPerSecond);
     event LogRewardsExpiration(uint256 rewardsExpiration);
 
-    /// @param _sushi The SUSHI token contract address.
-    constructor(address _sushi, address _firstOwner) public {
+    /// @param _rewardToken The reward token contract address.
+    /// @param _firstOwner Initial owner of the contract.
+    constructor(address _rewardToken, address _firstOwner) public {
         require(
-            _sushi != address(0)
+            _rewardToken != address(0)
             && _firstOwner != address(0),
             "MiniChefV2::Cannot construct with zero address"
         );
 
-        SUSHI = IERC20(_sushi);
+        REWARD = IERC20(_rewardToken);
         transferOwnership(_firstOwner);
     }
 
     /// @notice Returns the number of MCV2 pools.
     function poolLength() public view returns (uint256 pools) {
         pools = poolInfo.length;
+    }
+
+    /// @notice Returns list of all lpTokens for ease of interfacing
+    function lpTokens() external view returns (IERC20[] memory) {
+        return lpToken;
+    }
+
+    /// @notice Returns list of all pool infos for ease of interfacing
+    function poolInfos() external view returns (PoolInfo[] memory) {
+        return poolInfo;
     }
 
     /// @notice Returns the status of an address as a funder
@@ -148,7 +156,7 @@ contract MiniChefV2 is Ownable {
         poolInfo.push(PoolInfo({
             allocPoint: allocPoint.to64(),
             lastRewardTime: block.timestamp.to64(),
-            accSushiPerShare: 0
+            accRewardPerShare: 0
         }));
         addedTokens[address(_lpToken)] = true;
         emit PoolAdded(lpToken.length.sub(1), allocPoint, _lpToken, _rewarder);
@@ -177,7 +185,7 @@ contract MiniChefV2 is Ownable {
         }
     }
 
-    /// @notice Update the given pool's SUSHI allocation point and `IRewarder` contract. Can only be called by the owner.
+    /// @notice Update the given pool's reward allocation point and `IRewarder` contract. Can only be called by the owner.
     /// @param _pid The index of the pool. See `poolInfo`.
     /// @param _allocPoint New AP of the pool.
     /// @param _rewarder Address of the rewarder delegate.
@@ -218,14 +226,14 @@ contract MiniChefV2 is Ownable {
         emit Migrate(_pid);
     }
 
-    /// @notice View function to see pending SUSHI on frontend.
+    /// @notice View function to see pending reward on frontend.
     /// @param _pid The index of the pool. See `poolInfo`.
     /// @param _user Address of user.
-    /// @return pending SUSHI reward for a given user.
-    function pendingSushi(uint256 _pid, address _user) external view returns (uint256 pending) {
+    /// @return pending reward for a given user.
+    function pendingReward(uint256 _pid, address _user) external view returns (uint256 pending) {
         PoolInfo memory pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accSushiPerShare = pool.accSushiPerShare;
+        uint256 accRewardPerShare = pool.accRewardPerShare;
         uint256 lpSupply = lpToken[_pid].balanceOf(address(this));
         if (block.timestamp > pool.lastRewardTime && lpSupply != 0) {
             uint256 time = block.timestamp <= rewardsExpiration
@@ -233,10 +241,10 @@ contract MiniChefV2 is Ownable {
                 : rewardsExpiration > pool.lastRewardTime
                     ? rewardsExpiration.sub(pool.lastRewardTime) // Accrue rewards until expiration
                     : 0; // No rewards to accrue
-            uint256 sushiReward = time.mul(sushiPerSecond).mul(pool.allocPoint) / totalAllocPoint;
-            accSushiPerShare = accSushiPerShare.add(sushiReward.mul(ACC_SUSHI_PRECISION) / lpSupply);
+            uint256 reward = time.mul(rewardPerSecond).mul(pool.allocPoint) / totalAllocPoint;
+            accRewardPerShare = accRewardPerShare.add(reward.mul(ACC_REWARD_PRECISION) / lpSupply);
         }
-        pending = int256(user.amount.mul(accSushiPerShare) / ACC_SUSHI_PRECISION).sub(user.rewardDebt).toUInt256();
+        pending = int256(user.amount.mul(accRewardPerShare) / ACC_REWARD_PRECISION).sub(user.rewardDebt).toUInt256();
     }
 
     /// @notice Update reward variables for all pools. Be careful of gas spending!
@@ -269,16 +277,16 @@ contract MiniChefV2 is Ownable {
                     : rewardsExpiration > pool.lastRewardTime
                         ? rewardsExpiration.sub(pool.lastRewardTime) // Accrue rewards until expiration
                         : 0; // No rewards to accrue
-                uint256 sushiReward = time.mul(sushiPerSecond).mul(pool.allocPoint) / totalAllocPoint;
-                pool.accSushiPerShare = pool.accSushiPerShare.add((sushiReward.mul(ACC_SUSHI_PRECISION) / lpSupply).to128());
+                uint256 reward = time.mul(rewardPerSecond).mul(pool.allocPoint) / totalAllocPoint;
+                pool.accRewardPerShare = pool.accRewardPerShare.add((reward.mul(ACC_REWARD_PRECISION) / lpSupply).to128());
             }
             pool.lastRewardTime = block.timestamp.to64();
             poolInfo[pid] = pool;
-            emit PoolUpdate(pid, pool.lastRewardTime, lpSupply, pool.accSushiPerShare);
+            emit PoolUpdate(pid, pool.lastRewardTime, lpSupply, pool.accRewardPerShare);
         }
     }
 
-    /// @notice Deposit LP tokens to MCV2 for SUSHI allocation.
+    /// @notice Deposit LP tokens to MCV2 for reward allocation.
     /// @param pid The index of the pool. See `poolInfo`.
     /// @param amount LP token amount to deposit.
     /// @param to The receiver of `amount` deposit benefit.
@@ -288,17 +296,25 @@ contract MiniChefV2 is Ownable {
 
         // Effects
         user.amount = user.amount.add(amount);
-        user.rewardDebt = user.rewardDebt.add(int256(amount.mul(pool.accSushiPerShare) / ACC_SUSHI_PRECISION));
+        user.rewardDebt = user.rewardDebt.add(int256(amount.mul(pool.accRewardPerShare) / ACC_REWARD_PRECISION));
 
         // Interactions
         IRewarder _rewarder = rewarder[pid];
         if (address(_rewarder) != address(0)) {
-            _rewarder.onSushiReward(pid, to, to, 0, user.amount);
+            _rewarder.onReward(pid, to, to, 0, user.amount);
         }
 
         lpToken[pid].safeTransferFrom(msg.sender, address(this), amount);
 
         emit Deposit(msg.sender, pid, amount, to);
+    }
+
+    function depositWithPermit(uint256 pid, uint256 amount, address to, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        // permit
+        lpToken[pid].permit(msg.sender, address(this), amount, deadline, v, r, s);
+
+        // deposit
+        deposit(pid, amount, to);
     }
 
     /// @notice Withdraw LP tokens from MCV2.
@@ -310,13 +326,13 @@ contract MiniChefV2 is Ownable {
         UserInfo storage user = userInfo[pid][msg.sender];
 
         // Effects
-        user.rewardDebt = user.rewardDebt.sub(int256(amount.mul(pool.accSushiPerShare) / ACC_SUSHI_PRECISION));
+        user.rewardDebt = user.rewardDebt.sub(int256(amount.mul(pool.accRewardPerShare) / ACC_REWARD_PRECISION));
         user.amount = user.amount.sub(amount);
 
         // Interactions
         IRewarder _rewarder = rewarder[pid];
         if (address(_rewarder) != address(0)) {
-            _rewarder.onSushiReward(pid, msg.sender, to, 0, user.amount);
+            _rewarder.onReward(pid, msg.sender, to, 0, user.amount);
         }
 
         lpToken[pid].safeTransfer(to, amount);
@@ -326,55 +342,55 @@ contract MiniChefV2 is Ownable {
 
     /// @notice Harvest proceeds for transaction sender to `to`.
     /// @param pid The index of the pool. See `poolInfo`.
-    /// @param to Receiver of SUSHI rewards.
+    /// @param to Receiver of rewards.
     function harvest(uint256 pid, address to) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo storage user = userInfo[pid][msg.sender];
-        int256 accumulatedSushi = int256(user.amount.mul(pool.accSushiPerShare) / ACC_SUSHI_PRECISION);
-        uint256 _pendingSushi = accumulatedSushi.sub(user.rewardDebt).toUInt256();
+        int256 accumulatedReward = int256(user.amount.mul(pool.accRewardPerShare) / ACC_REWARD_PRECISION);
+        uint256 _pendingReward = accumulatedReward.sub(user.rewardDebt).toUInt256();
 
         // Effects
-        user.rewardDebt = accumulatedSushi;
+        user.rewardDebt = accumulatedReward;
 
         // Interactions
-        if (_pendingSushi != 0) {
-            SUSHI.safeTransfer(to, _pendingSushi);
+        if (_pendingReward != 0) {
+            REWARD.safeTransfer(to, _pendingReward);
         }
 
         IRewarder _rewarder = rewarder[pid];
         if (address(_rewarder) != address(0)) {
-            _rewarder.onSushiReward( pid, msg.sender, to, _pendingSushi, user.amount);
+            _rewarder.onReward( pid, msg.sender, to, _pendingReward, user.amount);
         }
 
-        emit Harvest(msg.sender, pid, _pendingSushi);
+        emit Harvest(msg.sender, pid, _pendingReward);
     }
 
     /// @notice Withdraw LP tokens from MCV2 and harvest proceeds for transaction sender to `to`.
     /// @param pid The index of the pool. See `poolInfo`.
     /// @param amount LP token amount to withdraw.
-    /// @param to Receiver of the LP tokens and SUSHI rewards.
+    /// @param to Receiver of the LP tokens and rewards.
     function withdrawAndHarvest(uint256 pid, uint256 amount, address to) public {
         PoolInfo memory pool = updatePool(pid);
         UserInfo storage user = userInfo[pid][msg.sender];
-        int256 accumulatedSushi = int256(user.amount.mul(pool.accSushiPerShare) / ACC_SUSHI_PRECISION);
-        uint256 _pendingSushi = accumulatedSushi.sub(user.rewardDebt).toUInt256();
+        int256 accumulatedReward = int256(user.amount.mul(pool.accRewardPerShare) / ACC_REWARD_PRECISION);
+        uint256 _pendingReward = accumulatedReward.sub(user.rewardDebt).toUInt256();
 
         // Effects
-        user.rewardDebt = accumulatedSushi.sub(int256(amount.mul(pool.accSushiPerShare) / ACC_SUSHI_PRECISION));
+        user.rewardDebt = accumulatedReward.sub(int256(amount.mul(pool.accRewardPerShare) / ACC_REWARD_PRECISION));
         user.amount = user.amount.sub(amount);
 
         // Interactions
-        SUSHI.safeTransfer(to, _pendingSushi);
+        REWARD.safeTransfer(to, _pendingReward);
 
         IRewarder _rewarder = rewarder[pid];
         if (address(_rewarder) != address(0)) {
-            _rewarder.onSushiReward(pid, msg.sender, to, _pendingSushi, user.amount);
+            _rewarder.onReward(pid, msg.sender, to, _pendingReward, user.amount);
         }
 
         lpToken[pid].safeTransfer(to, amount);
 
         emit Withdraw(msg.sender, pid, amount, to);
-        emit Harvest(msg.sender, pid, _pendingSushi);
+        emit Harvest(msg.sender, pid, _pendingReward);
     }
 
     /// @notice Withdraw without caring about rewards. EMERGENCY ONLY.
@@ -416,26 +432,26 @@ contract MiniChefV2 is Ownable {
     function fundRewards(uint256 funding, uint256 duration) external onlyFunder {
         require(funding > 0, "MiniChefV2: funding cannot be zero");
 
-        SUSHI.safeTransferFrom(msg.sender, address(this), funding);
+        REWARD.safeTransferFrom(msg.sender, address(this), funding);
 
         if (block.timestamp >= rewardsExpiration) {
             require(duration > 0, "MiniChefV2: reward duration cannot be zero");
             massUpdateAllPools();
             rewardsExpiration = block.timestamp.add(duration);
-            sushiPerSecond = funding / duration;
+            rewardPerSecond = funding / duration;
         } else {
             uint256 remainingTime = rewardsExpiration.sub(block.timestamp);
-            uint256 remainingRewards = remainingTime.mul(sushiPerSecond);
+            uint256 remainingRewards = remainingTime.mul(rewardPerSecond);
             uint256 newRewardsExpiration = rewardsExpiration.add(duration);
-            uint256 newSushiPerSecond = remainingRewards.add(funding) / (newRewardsExpiration.sub(block.timestamp));
-            if (newSushiPerSecond != sushiPerSecond) {
+            uint256 newRewardPerSecond = remainingRewards.add(funding) / (newRewardsExpiration.sub(block.timestamp));
+            if (newRewardPerSecond != rewardPerSecond) {
                 massUpdateAllPools();
             }
             rewardsExpiration = newRewardsExpiration;
-            sushiPerSecond = newSushiPerSecond;
+            rewardPerSecond = newRewardPerSecond;
         }
 
-        emit LogSushiPerSecond(sushiPerSecond);
+        emit LogRewardPerSecond(rewardPerSecond);
         emit LogRewardsExpiration(rewardsExpiration);
     }
 
@@ -447,11 +463,11 @@ contract MiniChefV2 is Ownable {
         massUpdateAllPools();
 
         uint256 remainingTime = rewardsExpiration.sub(block.timestamp);
-        uint256 remainingRewards = remainingTime.mul(sushiPerSecond);
+        uint256 remainingRewards = remainingTime.mul(rewardPerSecond);
         rewardsExpiration = block.timestamp.add(duration);
-        sushiPerSecond = remainingRewards / (rewardsExpiration.sub(block.timestamp));
+        rewardPerSecond = remainingRewards / (rewardsExpiration.sub(block.timestamp));
 
-        emit LogSushiPerSecond(sushiPerSecond);
+        emit LogRewardPerSecond(rewardPerSecond);
         emit LogRewardsExpiration(rewardsExpiration);
     }
 
@@ -461,12 +477,12 @@ contract MiniChefV2 is Ownable {
     function extendRewardsViaFunding(uint256 funding, uint256 minExtension) external {
         require(funding > 0, "MiniChefV2: funding amount cannot be zero");
 
-        uint256 extensionDuration = funding / sushiPerSecond;
+        uint256 extensionDuration = funding / rewardPerSecond;
         require(extensionDuration >= minExtension, "MiniChefV2: insufficient extension limit");
 
         rewardsExpiration = rewardsExpiration.add(extensionDuration);
 
-        SUSHI.safeTransferFrom(msg.sender, address(this), funding);
+        REWARD.safeTransferFrom(msg.sender, address(this), funding);
 
         emit LogRewardsExpiration(rewardsExpiration);
     }
@@ -477,12 +493,12 @@ contract MiniChefV2 is Ownable {
     function extendRewardsViaDuration(uint256 extension, uint256 maxFunding) external {
         require(extension > 0, "MiniChefV2: extension duration cannot be zero");
 
-        uint256 fundingRequired = sushiPerSecond.mul(extension);
+        uint256 fundingRequired = rewardPerSecond.mul(extension);
         require(fundingRequired <= maxFunding, "MiniChefV2: insufficient funding limit");
 
         rewardsExpiration = rewardsExpiration.add(extension);
 
-        SUSHI.safeTransferFrom(msg.sender, address(this), fundingRequired);
+        REWARD.safeTransferFrom(msg.sender, address(this), fundingRequired);
 
         emit LogRewardsExpiration(rewardsExpiration);
     }
