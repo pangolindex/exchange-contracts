df321a488d8e103f8a6e61ee053e1918e67c3ff1
Sunur Efe Vural <efe@efe.kim>
2022-01-06 19:54:42 -0500

    update RewarderComplex (under development)

--- contracts/dex/RewarderComplex.sol
+++ contracts/dex/RewarderComplex.sol
@@ -1,179 +1,101 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
-pragma experimental ABIEncoderV2;
 
 import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
 import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
-import "openzeppelin-contracts-legacy/access/Ownable.sol";
 
 import "./interfaces/IRewarder.sol";
 
-interface MiniChefV2 {
-    function lpToken(uint256 pid) external view returns (IERC20);
-}
-
-/// @author @0xKeno
-contract RewarderComplex is IRewarder, Ownable {
+contract RewarderSimple is IRewarder {
     using BoringMath for uint256;
-    using BoringMath128 for uint128;
     using BoringERC20 for IERC20;
 
-    IERC20 private immutable rewardToken;
+    IERC20[] private rewardTokens;
+    uint256[] private rewardMultipliers;
+    address private immutable CHEF_V2;
 
-    /// @notice Info of each MCV2 user.
-    /// `amount` LP token amount the user has provided.
-    /// `rewardDebt` The amount of SUSHI entitled to the user.
-    struct UserInfo {
-        uint256 amount;
-        uint256 rewardDebt;
-    }
+    /// @dev Should match the precision of the base reward token (PNG)
+    uint256 private constant BASE_REWARD_TOKEN_DIVISOR = 1e18;
 
-    /// @notice Info of each MCV2 pool.
-    /// `allocPoint` The amount of allocation points assigned to the pool.
-    /// Also known as the amount of SUSHI to distribute per block.
-    struct PoolInfo {
-        uint128 accSushiPerShare;
-        uint64 lastRewardBlock;
-        uint64 allocPoint;
-    }
+    /// @dev Additional reward quantities that might be owed to users trying to claim after funds have been exhausted
+    mapping(address => uint256[]) private rewardDebts;
 
-    /// @notice Info of each pool.
-    mapping (uint256 => PoolInfo) public poolInfo;
+    /// @param _rewardTokens The address of each additional reward token
+    /// @param _rewardMultipliers The amount of each additional reward token to be claimable for every 1 base reward (PNG) being claimed
+    /// @param _chefV2 The address of the chef contract where the base reward (PNG) is being emitted
+    /// @notice Each reward multiplier should have a precision matching that individual token
+    constructor (
+        address[] memory _rewardTokens,
+        uint256[] memory _rewardMultipliers,
+        address _chefV2
+    ) public {
+        require(
+            _rewardTokens.length > 0
+             && _rewardTokens.length == _rewardMultipliers.length,
+            "RewarderSimple::Invalid input lengths"
+        );
 
-    uint256[] public poolIds;
+        require(
+            _chefV2 != address(0),
+            "RewarderSimple::Invalid chef address"
+        );
 
-    /// @notice Info of each user that stakes LP tokens.
-    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
-    /// @dev Total allocation points. Must be the sum of all allocation points in all pools.
-    uint256 totalAllocPoint;
+        for (uint256 i; i < _rewardTokens.length; i++) {
+            require(_rewardTokens[i] != address(0), "RewarderSimple::Cannot reward zero address");
+            require(_rewardMultipliers[i] > 0, "RewarderSimple::Invalid multiplier");
 
-    uint256 public tokenPerBlock;
-    uint256 private constant ACC_TOKEN_PRECISION = 1e12;
-
-    address private immutable MASTERCHEF_V2;
-
-    event LogOnReward(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
-    event LogPoolAddition(uint256 indexed pid, uint256 allocPoint);
-    event LogSetPool(uint256 indexed pid, uint256 allocPoint);
-    event LogUpdatePool(uint256 indexed pid, uint64 lastRewardBlock, uint256 lpSupply, uint256 accSushiPerShare);
-    event LogInit();
-
-    constructor (IERC20 _rewardToken, uint256 _tokenPerBlock, address _MASTERCHEF_V2) public {
-        rewardToken = _rewardToken;
-        tokenPerBlock = _tokenPerBlock;
-        MASTERCHEF_V2 = _MASTERCHEF_V2;
-    }
-
-
-    function onSushiReward (uint256 pid, address _user, address to, uint256, uint256 lpToken) onlyMCV2 override external {
-        PoolInfo memory pool = updatePool(pid);
-        UserInfo storage user = userInfo[pid][_user];
-        uint256 pending;
-        if (user.amount > 0) {
-            pending =
-            (user.amount.mul(pool.accSushiPerShare) / ACC_TOKEN_PRECISION).sub(
-                user.rewardDebt
-            );
-            rewardToken.safeTransfer(to, pending);
+            rewardTokens[i] = IERC20(_rewardTokens[i]);
         }
-        user.amount = lpToken;
-        user.rewardDebt = lpToken.mul(pool.accSushiPerShare) / ACC_TOKEN_PRECISION;
-        emit LogOnReward(_user, pid, pending, to);
+
+        rewardMultipliers = _rewardMultipliers;
+        CHEF_V2 = _chefV2;
     }
 
-    function pendingTokens(uint256 pid, address user, uint256) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts) {
-        IERC20[] memory _rewardTokens = new IERC20[](1);
-        _rewardTokens[0] = (rewardToken);
-        uint256[] memory _rewardAmounts = new uint256[](1);
-        _rewardAmounts[0] = pendingToken(pid, user);
-        return (_rewardTokens, _rewardAmounts);
+    function onReward(uint256, address user, address to, uint256 rewardAmount, uint256) onlyMCV2 override external {
+        for (uint256 i; i < rewardTokens.length; i++) {
+            uint256 pendingReward = rewardDebts[user][i] + rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR;
+            uint256 rewardBal = rewardTokens[i].balanceOf(address(this));
+            if (pendingReward > rewardBal) {
+                rewardDebts[user][i] = pendingReward - rewardBal;
+                rewardTokens[i].safeTransfer(to, rewardBal);
+            } else {
+                rewardDebts[user][i] = 0;
+                rewardTokens[i].safeTransfer(to, pendingReward);
+            }
+        }
+    }
+
+    /// @notice Shows pending tokens that can be currently claimed
+    function pendingTokens(uint256, address user, uint256 rewardAmount) override external view returns (IERC20[] memory tokens, uint256[] memory amounts) {
+        for (uint256 i; i < rewardTokens.length; i++) {
+            uint256 pendingReward = rewardDebts[user][i] + rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR;
+            uint256 rewardBal = rewardTokens[i].balanceOf(address(this));
+            if (pendingReward > rewardBal) {
+                amounts[i] = rewardBal;
+            } else {
+                amounts[i] = pendingReward;
+            }
+        }
+        return (rewardTokens, amounts);
+    }
+
+    /// @notice Shows pending tokens including rewards accrued after the funding has been exhausted
+    /// @notice these extra rewards could be claimed if more funding is added to the contract
+    function pendingTokensDebt(uint256, address user, uint256 rewardAmount) external view returns (IERC20[] memory tokens, uint256[] memory amounts) {
+        for (uint256 i; i < rewardTokens.length; i++) {
+            uint256 pendingReward = rewardDebts[user][i] + rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR;
+            amounts[i] = pendingReward;
+        }
+        return (rewardTokens, amounts);
     }
 
     modifier onlyMCV2 {
         require(
-            msg.sender == MASTERCHEF_V2,
+            msg.sender == CHEF_V2,
             "Only MCV2 can call this function."
         );
         _;
-    }
-
-    /// @notice Returns the number of MCV2 pools.
-    function poolLength() public view returns (uint256 pools) {
-        pools = poolIds.length;
-    }
-
-    /// @notice Add a new LP to the pool.  Can only be called by the owner.
-    /// DO NOT add the same LP token more than once. Rewards will be messed up if you do.
-    /// @param allocPoint AP of the new pool.
-    /// @param _pid Pid on MCV2
-    function add(uint256 allocPoint, uint256 _pid) public onlyOwner {
-        require(poolInfo[_pid].lastRewardBlock == 0, "Pool already exists");
-        uint256 lastRewardBlock = block.number;
-        totalAllocPoint = totalAllocPoint.add(allocPoint);
-
-        poolInfo[_pid] = PoolInfo({
-        allocPoint: allocPoint.to64(),
-        lastRewardBlock: lastRewardBlock.to64(),
-        accSushiPerShare: 0
-        });
-        poolIds.push(_pid);
-        emit LogPoolAddition(_pid, allocPoint);
-    }
-
-    /// @notice Update the given pool's SUSHI allocation point and `IRewarder` contract. Can only be called by the owner.
-    /// @param _pid The index of the pool. See `poolInfo`.
-    /// @param _allocPoint New AP of the pool.
-    function set(uint256 _pid, uint256 _allocPoint) public onlyOwner {
-        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
-        poolInfo[_pid].allocPoint = _allocPoint.to64();
-        emit LogSetPool(_pid, _allocPoint);
-    }
-
-    /// @notice View function to see pending Token
-    /// @param _pid The index of the pool. See `poolInfo`.
-    /// @param _user Address of user.
-    /// @return pending SUSHI reward for a given user.
-    function pendingToken(uint256 _pid, address _user) public view returns (uint256 pending) {
-        PoolInfo memory pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][_user];
-        uint256 accSushiPerShare = pool.accSushiPerShare;
-        uint256 lpSupply = MiniChefV2(MASTERCHEF_V2).lpToken(_pid).balanceOf(MASTERCHEF_V2);
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 blocks = block.number.sub(pool.lastRewardBlock);
-            uint256 sushiReward = blocks.mul(tokenPerBlock).mul(pool.allocPoint) / totalAllocPoint;
-            accSushiPerShare = accSushiPerShare.add(sushiReward.mul(ACC_TOKEN_PRECISION) / lpSupply);
-        }
-        pending = (user.amount.mul(accSushiPerShare) / ACC_TOKEN_PRECISION).sub(user.rewardDebt);
-    }
-
-    /// @notice Update reward variables for all pools. Be careful of gas spending!
-    /// @param pids Pool IDs of all to be updated. Make sure to update all active pools.
-    function massUpdatePools(uint256[] calldata pids) external {
-        uint256 len = pids.length;
-        for (uint256 i = 0; i < len; ++i) {
-            updatePool(pids[i]);
-        }
-    }
-
-    /// @notice Update reward variables of the given pool.
-    /// @param pid The index of the pool. See `poolInfo`.
-    /// @return pool Returns the pool that was updated.
-    function updatePool(uint256 pid) public returns (PoolInfo memory pool) {
-        pool = poolInfo[pid];
-        require(pool.lastRewardBlock != 0, "Pool does not exist");
-        if (block.number > pool.lastRewardBlock) {
-            uint256 lpSupply = MiniChefV2(MASTERCHEF_V2).lpToken(pid).balanceOf(MASTERCHEF_V2);
-
-            if (lpSupply > 0) {
-                uint256 blocks = block.number.sub(pool.lastRewardBlock);
-                uint256 sushiReward = blocks.mul(tokenPerBlock).mul(pool.allocPoint) / totalAllocPoint;
-                pool.accSushiPerShare = pool.accSushiPerShare.add((sushiReward.mul(ACC_TOKEN_PRECISION) / lpSupply).to128());
-            }
-            pool.lastRewardBlock = block.number.to64();
-            poolInfo[pid] = pool;
-            emit LogUpdatePool(pid, pool.lastRewardBlock, lpSupply, pool.accSushiPerShare);
-        }
     }
 
 }
