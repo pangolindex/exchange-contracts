2c435f50be5eeed6ce02cdca1aea6ae04bf7ef75
Sunur Efe Vural <efe@efe.kim>
2022-01-06 19:49:36 -0500

    update IRewarder

--- contracts/governance/interfaces/IRewarder.sol
+++ contracts/governance/interfaces/IRewarder.sol
@@ -2,8 +2,9 @@
 
 pragma solidity 0.6.12;
 import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+
 interface IRewarder {
     using BoringERC20 for IERC20;
-    function onSushiReward(uint256 pid, address user, address recipient, uint256 sushiAmount, uint256 newLpAmount) external;
-    function pendingTokens(uint256 pid, address user, uint256 sushiAmount) external view returns (IERC20[] memory, uint256[] memory);
+    function onReward(uint256 pid, address user, address recipient, uint256 rewardAmount, uint256 newLpAmount) external;
+    function pendingTokens(uint256 pid, address user, uint256 rewardAmount) external view returns (IERC20[] memory, uint256[] memory);
 }
