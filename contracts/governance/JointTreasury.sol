23e4f46bf6db7ad203590e1e3b152299a784df15
Sunur Efe Vural <efe@efe.kim>
2022-01-09 12:07:08 -0500

    reorder functions, rename internal variable

--- contracts/governance/JointTreasury.sol
+++ contracts/governance/JointTreasury.sol
@@ -8,7 +8,8 @@ contract JointTreasury {
 
     address public admin;
     uint256 public constant ALLOCATION_DENOMINATOR = 1000;
-    uint8 internal numberOfBeneficiaries;
+
+    uint8 internal _numberOfBeneficiaries;
 
     mapping(address => uint256) internal _allocations;
     mapping(uint8 => address) internal _beneficiaries;
@@ -23,35 +24,10 @@ contract JointTreasury {
         setBeneficiaries(newBeneficiaries);
     }
 
-    function setAdmin(address newAdmin) public {
-        require(msg.sender == admin, "sender not admin");
-        admin = newAdmin;
-        emit adminChanged(admin);
-    }
-
-    function setBeneficiaries(Beneficiary[] memory newBeneficiaries) public {
-        if (numberOfBeneficiaries != 0) {
-            require(msg.sender == admin, "sender not admin");
-        }
-        numberOfBeneficiaries = uint8(newBeneficiaries.length);
-        uint256 allocations;
-        for (uint8 i; i < numberOfBeneficiaries; i++) {
-            Beneficiary memory beneficiary = newBeneficiaries[i];
-            _allocations[beneficiary.account] = beneficiary.allocation;
-            _beneficiaries[i] = beneficiary.account;
-            allocations += beneficiary.allocation;
-        }
-        require(
-            allocations == ALLOCATION_DENOMINATOR,
-            "total allocations does not equal to denominator"
-        );
-        emit beneficiariesChanged(newBeneficiaries);
-    }
-
     function getBeneficiaries() external view returns (Beneficiary[] memory) {
         Beneficiary[] memory beneficiaries =
-            new Beneficiary[](numberOfBeneficiaries);
-        for (uint8 i; i < numberOfBeneficiaries; i++) {
+            new Beneficiary[](_numberOfBeneficiaries);
+        for (uint8 i; i < _numberOfBeneficiaries; i++) {
             address account = _beneficiaries[i];
             beneficiaries[i] = Beneficiary({
                 account: account, allocation: _allocations[account]
@@ -62,7 +38,7 @@ contract JointTreasury {
 
     function distributeToken(address token) public {
         uint256 amount = IERC20(token).balanceOf(address(this));
-        for (uint8 i; i < numberOfBeneficiaries; i++) {
+        for (uint8 i; i < _numberOfBeneficiaries; i++) {
             address receiverAddress = _beneficiaries[i];
             IERC20(token).safeTransfer(
                 receiverAddress,
@@ -70,6 +46,31 @@ contract JointTreasury {
             );
         }
         emit tokenDistributed(token, amount);
+    }
+
+    function setAdmin(address newAdmin) public {
+        require(msg.sender == admin, "sender not admin");
+        admin = newAdmin;
+        emit adminChanged(admin);
+    }
+
+    function setBeneficiaries(Beneficiary[] memory newBeneficiaries) public {
+        if (_numberOfBeneficiaries != 0) {
+            require(msg.sender == admin, "sender not admin");
+        }
+        _numberOfBeneficiaries = uint8(newBeneficiaries.length);
+        uint256 allocations;
+        for (uint8 i; i < _numberOfBeneficiaries; i++) {
+            Beneficiary memory beneficiary = newBeneficiaries[i];
+            _allocations[beneficiary.account] = beneficiary.allocation;
+            _beneficiaries[i] = beneficiary.account;
+            allocations += beneficiary.allocation;
+        }
+        require(
+            allocations == ALLOCATION_DENOMINATOR,
+            "total allocations does not equal to denominator"
+        );
+        emit beneficiariesChanged(newBeneficiaries);
     }
 
     event tokenDistributed(address token, uint256 amount);
