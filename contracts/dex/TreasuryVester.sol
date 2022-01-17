ccf7f6c7b8ebdd41a3f87418c7c82541b72280b8
Sunur Efe Vural <efe@efe.kim>
2022-01-17 15:24:24 -0500

    remove unused library

--- contracts/dex/TreasuryVester.sol
+++ contracts/dex/TreasuryVester.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract TreasuryVester is Ownable {
+contract TreasuryVester {
     using SafeERC20 for IERC20;
 
     struct Recipient{
