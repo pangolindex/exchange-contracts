c490c39108608db286a429085c93b615f0895bff
Sunur Efe Vural <efe@efe.kim>
2022-01-17 15:23:57 -0500

    remove unused library

--- contracts/dex/TreasuryVester.sol
+++ contracts/dex/TreasuryVester.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
