9a11b95420b1f52b4dc48b5eaeadbdd65483a34c
Justin Trollip <jtrollip@protonmail.com>
2022-01-18 13:32:08 +1000

    Added some extra chains

--- hardhat.config.js
+++ hardhat.config.js
@@ -64,6 +64,12 @@ module.exports = {
       chainId: 43112,
       initialDate: "2020-10-10",
     },
+    ethereum: {
+      url: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      gasPrice: 470000000000,
+      chainId: 1,
+      accounts: ["0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"]
+    },
     avash: {
       url: 'http://localhost:9650/ext/bc/C/rpc',
       gasPrice: 470000000000,
@@ -76,6 +82,24 @@ module.exports = {
       chainId: 43113,
       accounts: []
     },
+    arbitrum_mainnet: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      gasPrice: 470000000000,
+      chainId: 42161,
+      accounts: []
+    },
+    aurora_mainnet: {
+      url: 'https://mainnet.aurora.dev',
+      gasPrice: 470000000000,
+      chainId: 1313161554,
+      accounts: []
+    },
+    aurora_testnet: {
+      url: 'https://testnet.aurora.dev/',
+      gasPrice: 470000000000,
+      chainId: 1313161555,
+      accounts: []
+    },
     avalanche_mainnet: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
       gasPrice: 470000000000,
@@ -83,15 +107,33 @@ module.exports = {
       accounts: []
     },
     bsc_mainnet: {
-      url: 'https://bsc-dataseed.binance.org/ ',
+      url: 'https://bsc-dataseed.binance.org/',
       gasPrice: 470000000000,
       chainId: 56,
+      accounts: []
+    },
+    bsc_testnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      gasPrice: 470000000000,
+      chainId: 97,
+      accounts: []
+    },
+    cronos_mainnet: {
+      url: 'https://evm-cronos.crypto.org/',
+      gasPrice: 470000000000,
+      chainId: 25,
       accounts: []
     },
     fantom_mainnet: {
       url: 'https://rpc.ftm.tools/',
       gasPrice: 470000000000,
       chainId: 250,
+      accounts: []
+    },
+    fantom_testnet: {
+      url: 'https://rpc.testnet.fantom.network/',
+      gasPrice: 470000000000,
+      chainId: 0xfa2,
       accounts: []
     },
     fuse_mainnet: {
@@ -112,10 +154,22 @@ module.exports = {
       chainId: 128,
       accounts: []
     },
+    klaytn_mainnet: {
+      url: '	https://kaikas.cypress.klaytn.net:8651',
+      gasPrice: 470000000000,
+      chainId: 8217,
+      accounts: []
+    },
     moonriver_mainnet: {
       url: 'https://rpc.moonriver.moonbeam.network',
       gasPrice: 470000000000,
       chainId: 1285,
+      accounts: []
+    },
+    moonbeam_mainnet: {
+      url: 'https://rpc.api.moonbeam.network',
+      gasPrice: 470000000000,
+      chainId: 1284,
       accounts: []
     },
     okex_mainnet: {
@@ -134,6 +188,12 @@ module.exports = {
       url: 'https://polygon-rpc.com/ ',
       gasPrice: 470000000000,
       chainId: 137,
+      accounts: []
+    },
+    polygon_testnet: {
+      url: 'https://rpc-mumbai.maticvigil.com/',
+      gasPrice: 470000000000,
+      chainId: 80001,
       accounts: []
     },
     xdai_mainnet: {
