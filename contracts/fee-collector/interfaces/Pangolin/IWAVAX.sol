445ccea094ef4a004ffe409533ca5313980a46a2
Sunur Efe Vural <efe@efe.kim>
2022-01-24 11:39:52 -0500

    Revert "Added Fee Collector"
    This reverts commit ab14a5eaa0f0f200dbe536d184592609a71e66e1.
    fee-collector already included in contracts/dex

--- contracts/fee-collector/interfaces/Pangolin/IWAVAX.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-interface IWAVAX {
-    function deposit() external payable;
-    function transfer(address to, uint value) external returns (bool);
-    function withdraw(uint) external;
-    function balanceOf(address owner) external view returns (uint);
-}
