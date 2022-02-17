5a2fe2ffc92432e3fd998ff75ee0251ca600e724
Sunur Efe Vural <efe@efe.kim>
2022-02-17 14:05:53 -0500

    rm unused library

--- contracts/amm/pangolin-periphery/PangolinLibraryProxy.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.5.0;
-
-import './libraries/PangolinLibrary.sol';
-
-contract PangolinLibraryProxy {
-    function pairFor(address factory, address tokenA, address tokenB) external pure returns (address pair) {
-        pair = PangolinLibrary.pairFor(factory, tokenA, tokenB);
-    }
-}
