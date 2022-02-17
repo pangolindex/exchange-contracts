305b11531a9f7da8e28baee91d6911b1e810c556
Sunur Efe Vural <efe@efe.kim>
2022-02-17 09:33:19 -0500

    adjust airdrop for new png

--- test/Airdrop.js
+++ test/Airdrop.js
@@ -21,7 +21,7 @@ describe('Airdrop', function () {
     });
 
     beforeEach(async function () {
-        this.png = await this.PNG.deploy(TOTAL_SUPPLY, this.admin.address, "PNG", "Pangolin");
+        this.png = await this.PNG.deploy(TOTAL_SUPPLY, AIRDROP_SUPPLY, "PNG", "Pangolin");
         await this.png.deployed();
         this.airdrop = await this.Airdrop.deploy(AIRDROP_SUPPLY, this.png.address, this.admin.address, TREASURY);
         await this.airdrop.deployed();
