ce8db23dbb4f30452a1eba1a0add653f249399ca
Sunur Efe Vural <efe@efe.kim>
2022-03-04 16:10:22 -0500

    change default incentive and treasury fee

--- contracts/dex/FeeCollector.sol
+++ contracts/dex/FeeCollector.sol
@@ -23,8 +23,8 @@ contract PangolinFeeCollector is Ownable {
 
     address public stakingRewards;
     address public treasury; // Joint Treasury
-    uint256 public harvestIncentive = 50;
-    uint256 public treasuryFee = 1500;
+    uint256 public harvestIncentive = 10;
+    uint256 public treasuryFee = 3000;
     address public miniChef;
     uint256 public miniChefPoolId;
 
