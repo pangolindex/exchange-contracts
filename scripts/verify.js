59744cd563df71ab841c4bd6f69a3a9c61566362
Sunur Efe Vural <efe@efe.kim>
2022-03-04 19:55:25 -0500

    add verification script

---
+++ scripts/verify.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+const { ADDRESSES } = require(`../addresses/${network.name}.js`);
+
+async function main() {
+    for (let i = 0; i < ADDRESSES.length; i++) {
+        await hre
+            .run("verify:verify", {
+                address: ADDRESSES[i].address,
+                constructorArguments: ADDRESSES[i].args,
+            })
+            .catch((error) => {
+                console.error(error);
+                console.log("SKIPPING TO THE NEXT CONTRACT!!!");
+                //process.exit(1);
+            });
+    }
+}
+
+main();
