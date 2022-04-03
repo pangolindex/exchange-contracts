5eb3c70d856594030fdce96915da028ff2d63d86
Sunur Efe Vural <efe@efe.kim>
2022-04-03 16:50:56 -0400

    add compound lp method when pair is avax

--- contracts/sunshine-staking/SunshineAndRainbowsCompoundLPAVAX.sol
+++ contracts/sunshine-staking/SunshineAndRainbowsCompoundLPAVAX.sol
@@ -2,26 +2,21 @@
 // solhint-disable not-rely-on-time
 pragma solidity ^0.8.0;
 
-import "./SunshineAndRainbowsCompound.sol";
+import "./SunshineAndRainbowsCompoundLP.sol";
 
-import "../pangolin-core/interfaces/IPangolinPair.sol";
-import "../pangolin-periphery/interfaces/IPangolinRouter.sol";
+import "../pangolin-periphery/interfaces/IWAVAX.sol";
 
 /**
  * @title Sunshine and Rainbows Extension: LP Stake Compound
  * @notice An extension to `SunshineAndRainbows` that implements locked-stake
- * harvesting feature when the staking tokens is a pool token and the reward
- * token is one of the tokens in the LP pair
+ * harvesting feature when the staking tokens is a pool token, the reward token
+ * is one of the tokens in the LP pair, and the paired tokens is WAVAX/WETH
  * @author shung for Pangolin
  */
-contract SunshineAndRainbowsCompoundLP is SunshineAndRainbowsCompound {
+contract SunshineAndRainbowsCompoundLPAVAX is SunshineAndRainbowsCompoundLP {
     using SafeERC20 for IERC20;
 
-    IPangolinRouter public immutable router;
-
-    IERC20 internal _pairToken;
-
-    bool internal _rewardTokenIs0;
+    IWAVAX private immutable _wrappedNativeToken;
 
     /**
      * @notice Constructs a new SunshineAndRainbows staking contract with
@@ -30,30 +25,25 @@ contract SunshineAndRainbowsCompoundLP is SunshineAndRainbowsCompound {
      * @param newRewardRegulator Contract address of the reward regulator which
      * distributes reward tokens
      * @param newRouter Address of the router that will be used to deposit
+     * @param newWrappedNativeToken Wrapping contract of the network's gas token
      */
     constructor(
         address newStakingToken,
         address newRewardRegulator,
-        address newRouter
-    ) SunshineAndRainbowsCompound(newStakingToken, newRewardRegulator) {
-        require(newRouter != address(0), "SAR::Constructor: zero address");
-        router = IPangolinRouter(newRouter);
-
-        IPangolinPair pair = IPangolinPair(newStakingToken);
-        IERC20 newRewardToken = IRewardRegulator(newRewardRegulator)
-            .rewardToken();
-
-        if (pair.token0() == address(newRewardToken)) {
-            _pairToken = IERC20(pair.token1());
-            _rewardTokenIs0 = true;
-        } else if (pair.token1() == address(newRewardToken)) {
-            _pairToken = IERC20(pair.token0());
-        } else {
-            revert("SAR::Constructor: Reward token not part of LP token");
-        }
-
-        newRewardToken.approve(address(router), type(uint).max);
-        _pairToken.approve(address(router), type(uint).max);
+        address newRouter,
+        address newWrappedNativeToken
+    )
+        SunshineAndRainbowsCompoundLP(
+            newStakingToken,
+            newRewardRegulator,
+            newRouter
+        )
+    {
+        require(
+            newWrappedNativeToken != address(0),
+            "SAR::Constructor: zero address"
+        );
+        _wrappedNativeToken = IWAVAX(newWrappedNativeToken);
     }
 
     /**
@@ -63,10 +53,8 @@ contract SunshineAndRainbowsCompoundLP is SunshineAndRainbowsCompound {
      * The new position also requires equal amount of pair token to the reward
      * amount
      * @param posId ID of the parent position whose rewards are harvested
-     * @param maxPairAmount The max amount of pair token that can be paired
-     * with rewards
      */
-    function compound(uint posId, uint maxPairAmount) external nonReentrant {
+    function compoundAVAX(uint posId) external payable nonReentrant {
         // update the state variables that govern the reward distribution
         _updateRewardVariables();
 
@@ -77,7 +65,7 @@ contract SunshineAndRainbowsCompoundLP is SunshineAndRainbowsCompound {
         children[childPosId] = Child(posId, block.timestamp);
 
         // harvest parent position and add liquidity with that
-        uint amount = _addLiquidity(_harvestWithDebt(posId), maxPairAmount);
+        uint amount = _addLiquidityAVAX(_harvestWithDebt(posId));
 
         // stake parent position rewards to child position
         _open(amount, address(this));
@@ -88,31 +76,29 @@ contract SunshineAndRainbowsCompoundLP is SunshineAndRainbowsCompound {
     /**
      * @notice Adds liquidity with the harvested reward & supplied pair
      * @param reward The amount of reward tokens harvested
-     * @param maxPairAmount The max amount of pair token that can be paired
-     * with reward
      */
-    function _addLiquidity(uint reward, uint maxPairAmount)
-        private
-        returns (uint)
-    {
-        require(reward != 0, "SAR::_addLiquidity: no reward");
+    function _addLiquidityAVAX(uint reward) private returns (uint) {
+        require(reward != 0, "SAR::_addLiquidityAVAX: no reward");
         IPangolinPair pair = IPangolinPair(address(stakingToken));
         (uint reserve0, uint reserve1, ) = pair.getReserves();
         require(
             reserve0 > 1000 && reserve1 > 1000,
-            "SAR::_addLiquidity: low reserves"
+            "SAR::_addLiquidityAVAX: low reserves"
         );
 
         uint pairAmount = _rewardTokenIs0
             ? (reward * reserve1) / reserve0
             : (reward * reserve0) / reserve1;
 
-        require(
-            maxPairAmount >= pairAmount,
-            "SAR::_addLiquidity: high slippage"
-        );
+        if (msg.value > pairAmount) {
+            unchecked {
+                require(payable(msg.sender).send(msg.value - pairAmount));
+            }
+        } else if (msg.value < pairAmount) {
+            revert("SAR::_addLiquidityAVAX: high slippage");
+        }
 
-        _pairToken.safeTransferFrom(msg.sender, address(this), pairAmount);
+        _wrappedNativeToken.deposit{ value: pairAmount }();
 
         (, , uint amount) = router.addLiquidity(
             address(rewardToken), // tokenA
