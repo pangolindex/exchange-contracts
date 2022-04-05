b0f60ade8c59401820a0d46582639211d1800b1e
Brandon <brandon@pangolin.exchange>
2022-04-05 16:55:40 -0400

    Updates
    Role based system
    Pausable instead of flag
    Immutable params
    Private functions

--- contracts/fee-collector/PangolinFeeCollectorV2.sol
+++ contracts/fee-collector/PangolinFeeCollectorV2.sol
@@ -2,35 +2,38 @@
 
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
+import "../mini-chef/IMiniChef.sol";
 import "../pangolin-core/interfaces/IPangolinPair.sol";
 import "../pangolin-periphery/interfaces/IPangolinRouter.sol";
 import "../staking-rewards/IStakingRewards.sol";
-import "../mini-chef/IMiniChef.sol";
 
-contract PangolinFeeCollectorV2 is Ownable {
+contract PangolinFeeCollectorV2 is AccessControl, Pausable {
 
     using SafeERC20 for IERC20;
     using Address for address;
 
-    address public constant FACTORY = 0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
-    address public constant ROUTER = 0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106;
-    address public constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    address public immutable FACTORY;
+    address public immutable ROUTER;
+    address public immutable WRAPPED_TOKEN;
     address public immutable GOVERNOR;
+    
+    bytes32 public constant ADMIN = 0x00;
+    bytes32 public constant HARVESTER = keccak256("HARVESTER");
 
-    uint256 public constant FEE_DENOMINATOR = 10000;
-    uint256 public constant MAX_HARVEST_INCENTIVE = 200;
+    uint256 public constant FEE_DENOMINATOR = 10_000;
+    uint256 public constant MAX_HARVEST_INCENTIVE = 200; // 2%
 
     address public stakingRewards;
     address public stakingRewardsRewardToken;
 
     address public treasury;
-    uint256 public treasuryFee = 1500;
-    uint256 public harvestIncentive = 10;
-    bool public harvestEnabledFlag = true;
+    uint256 public treasuryFee = 1500; // 15%
+    uint256 public harvestIncentive = 10; // 0.1%
 
     address public miniChef;
     uint256 public miniChefPoolId;
@@ -40,25 +43,34 @@ contract PangolinFeeCollectorV2 is Ownable {
 
     constructor(
         address _stakingRewards,
+        address _router,
+        address _factory,
         address _miniChef,
         uint256 _pid,
         address _governor,
-        address _treasury
+        address _wrappedToken,
+        address _treasury,
+        address _admin
     ) {
         require(_stakingRewards != address(0), "Invalid address");
         address _stakingRewardsRewardToken = IStakingRewards(_stakingRewards).rewardsToken();
         require(_stakingRewardsRewardToken != address(0), "Invalid staking reward");
         stakingRewards = _stakingRewards;
         stakingRewardsRewardToken = _stakingRewardsRewardToken;
+        ROUTER = _router;
+        FACTORY = _factory;
         miniChef = _miniChef;
         miniChefPoolId = _pid;
         GOVERNOR = _governor;
+        WRAPPED_TOKEN = _wrappedToken;
         treasury = _treasury;
+        _grantRole(ADMIN, _admin);
+        _grantRole(HARVESTER, _admin);
     }
 
     /// @notice Change staking rewards contract address
     /// @param _stakingRewards - New contract address
-    function setRewardsContract(address _stakingRewards) external onlyOwner {
+    function setRewardsContract(address _stakingRewards) external onlyRole(ADMIN) {
         require(_stakingRewards != address(0), "Invalid address");
         address _stakingRewardsRewardToken = IStakingRewards(_stakingRewards).rewardsToken();
         require(_stakingRewardsRewardToken != address(0), "Invalid staking reward");
@@ -68,16 +80,20 @@ contract PangolinFeeCollectorV2 is Ownable {
 
     /// @notice Change the percentage of each harvest that goes to the caller
     /// @param _harvestIncentive - New incentive ratio in bips
-    function setHarvestIncentive(uint256 _harvestIncentive) external onlyOwner {
+    function setHarvestIncentive(uint256 _harvestIncentive) external onlyRole(ADMIN) {
         require(_harvestIncentive <= MAX_HARVEST_INCENTIVE, "Incentive too large");
         require(_harvestIncentive + treasuryFee <= FEE_DENOMINATOR, "Total fees must <= 100");
         harvestIncentive = _harvestIncentive;
     }
 
-    /// @notice Enable or disable the harvest function for non-owners
-    /// @param flag - True to allow and False to disallow
-    function setHarvestEnabledFlag(bool flag) external onlyOwner {
-        harvestEnabledFlag = flag;
+    /// @notice Disable the harvest function for non-admins
+    function pauseHarvesting() external onlyRole(ADMIN) {
+        _pause();
+    }
+
+    /// @notice Re-enable the harvest function for non-admins
+    function unpauseHarvesting() external onlyRole(ADMIN) {
+        _unpause();
     }
 
     /// @notice Change the percentage of each harvest that goes to the treasury
@@ -91,33 +107,33 @@ contract PangolinFeeCollectorV2 is Ownable {
 
     /// @notice Updates the recipient of treasury fees
     /// @param _treasury - New treasury wallet
-    function setTreasury(address _treasury) external onlyOwner {
+    function setTreasury(address _treasury) external onlyRole(ADMIN) {
         require(_treasury != address(0));
         treasury = _treasury;
     }
 
     /// @notice Sets the MiniChef address to collect rewards from
     /// @param _miniChef - New MiniChef address
-    function setMiniChef(address _miniChef) external onlyOwner {
+    function setMiniChef(address _miniChef) external onlyRole(ADMIN) {
         require(_miniChef != address(0));
         miniChef = _miniChef;
     }
 
     /// @notice Sets the MiniChef pool used to accumulate rewards from emissions
     /// @param _pid - ID of the pool on MiniChef
-    function setMiniChefPool(uint256 _pid) external onlyOwner {
+    function setMiniChefPool(uint256 _pid) external onlyRole(ADMIN) {
         miniChefPoolId = _pid;
     }
 
     /// @notice Proxy function to set reward duration on the staking contract
     /// @param _rewardsDuration - the duration of the new period
-    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
+    function setRewardsDuration(uint256 _rewardsDuration) external onlyRole(ADMIN) {
         IStakingRewards(stakingRewards).setRewardsDuration(_rewardsDuration);
     }
 
     /// @notice Proxy function to change ownership of the staking contract
     /// @param _newOwner - address to transfer ownership to
-    function transferStakingOwnership(address _newOwner) external onlyOwner {
+    function transferStakingOwnership(address _newOwner) external onlyRole(ADMIN) {
         IStakingRewards(stakingRewards).transferOwnership(_newOwner);
     }
 
@@ -126,7 +142,7 @@ contract PangolinFeeCollectorV2 is Ownable {
     /// @param balance - The amount to pull
     /// @return amount0 - Amount of token0 received via burn
     /// @return amount1 - Amount of token1 received via burn
-    function _pullLiquidity(address pair, uint256 balance) internal returns (uint256 amount0, uint256 amount1) {
+    function _pullLiquidity(address pair, uint256 balance) private returns (uint256 amount0, uint256 amount1) {
         IERC20(pair).safeTransfer(pair, balance);
         (amount0, amount1) = IPangolinPair(pair).burn(address(this));
     }
@@ -134,18 +150,18 @@ contract PangolinFeeCollectorV2 is Ownable {
     /// @notice Swap a token for the specified output token
     /// @param token - address of the token to swap
     /// @param amount - amount of the token to swap
-    /// @dev Swaps are executed via pairs
-    function _swap(address token, address outputToken, uint256 amount) internal {
+    /// @dev Swaps are executed via router with infinite slippage tolerance
+    function _swap(address token, address outputToken, uint256 amount) private {
         address[] memory path;
 
-        if (token == WAVAX || outputToken == WAVAX) {
+        if (token == WRAPPED_TOKEN || outputToken == WRAPPED_TOKEN) {
             path = new address[](2);
             path[0] = token;
             path[1] = outputToken;
         } else {
             path = new address[](3);
             path[0] = token;
-            path[1] = WAVAX;
+            path[1] = WRAPPED_TOKEN;
             path[2] = outputToken;
         }
 
@@ -168,7 +184,7 @@ contract PangolinFeeCollectorV2 is Ownable {
     /// to the same previously specified output token
     /// @param liquidityPairs - list of all the pairs to pull
     /// @param outputToken - the token into which all liquidity will be swapped
-    function _collectFees(IPangolinPair[] memory liquidityPairs, address outputToken) internal {
+    function _collectFees(IPangolinPair[] memory liquidityPairs, address outputToken) private {
         for (uint256 i; i < liquidityPairs.length; ++i) {
             IPangolinPair liquidityPair = liquidityPairs[i];
             uint256 pglBalance = liquidityPair.balanceOf(address(this));
@@ -190,18 +206,17 @@ contract PangolinFeeCollectorV2 is Ownable {
     /// @notice - Converts all the LP tokens specified to the rewards token and
     /// transfers it to the staking contract
     /// @param liquidityPairs - list of all the pairs to harvest
-    /// @param claimMiniChef - whether to also harvest additional rewards
-    /// accrued via MiniChef
+    /// @param claimMiniChef - whether to also harvest additional rewards accrued via MiniChef
     function harvest(IPangolinPair[] memory liquidityPairs, bool claimMiniChef) external {
-        if (msg.sender != owner()) {
-            require(harvestEnabledFlag, "Harvest disabled");
+        if (!hasRole(HARVESTER, msg.sender)) {
+            // Enforce these conditions for callers without the HARVESTER role:
+            require(!paused(), "Harvest disabled");
             require(!address(msg.sender).isContract() && msg.sender == tx.origin, "No contracts");
         }
 
         address _stakingRewardsRewardToken = stakingRewardsRewardToken; // Gas savings
 
         if (liquidityPairs.length > 0) {
-            require(liquidityPairs.length < 51, "50 pairs max");
             _collectFees(liquidityPairs, _stakingRewardsRewardToken);
         }
 
@@ -230,7 +245,7 @@ contract PangolinFeeCollectorV2 is Ownable {
 
     // Migrated from PangolinLibrary
     // calculates the CREATE2 address for a Pangolin pair without making any external calls
-    function pairFor(address tokenA, address tokenB) internal pure returns (address pair) {
+    function pairFor(address tokenA, address tokenB) private pure returns (address pair) {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         pair = address(uint160(uint256(keccak256(abi.encodePacked(
             hex'ff',
