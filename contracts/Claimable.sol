bcd78bb6a9401ea88ae64002721e481156615c88
Sunur Efe Vural <efe@efe.kim>
2022-04-25 17:28:02 -0400

    update claimable to fit OZ standard

--- contracts/Claimable.sol
+++ contracts/Claimable.sol
@@ -3,15 +3,23 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Claimable is Ownable {
+abstract contract Claimable is Ownable {
     address private _pendingOwner;
+
+    event PendingOwnerSet(address indexed pendingOwner);
 
     function claimOwnership() external {
         require(_msgSender() == _pendingOwner, "Claimable: not pending owner");
         _transferOwnership(_pendingOwner);
+        delete _pendingOwner;
     }
 
     function transferOwnership(address newOwner) public override onlyOwner {
         _pendingOwner = newOwner;
+        emit PendingOwnerSet(newOwner);
+    }
+
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
     }
 }
