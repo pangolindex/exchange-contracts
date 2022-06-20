40b9a58fc5f3477d4475f3bcd37d8b8d848cf922
Justin Trollip <jtrollip@protonmail.com>
2022-06-20 13:41:53 +1000

    Adjusted Coston script

--- constants/coston_testnet.js
+++ constants/coston_testnet.js
@@ -1,7 +1,7 @@
-const WONE = "0x3Ee7094DADda15810F191DD6AcF7E4FFa37571e4";
-exports.WRAPPED_NATIVE_TOKEN = WONE;
-exports.PNG_SYMBOL = "cosPNG";
-exports.PNG_NAME = "Pangolin";
+const WCFLR = "0x3Ee7094DADda15810F191DD6AcF7E4FFa37571e4";
+exports.WRAPPED_NATIVE_TOKEN = WCFLR;
+exports.PNG_SYMBOL = "PCT";
+exports.PNG_NAME = "Pangolin Coston";
 exports.TOTAL_SUPPLY = 230000000; // 230M
 exports.AIRDROP_AMOUNT = 11500000; // 11.5M or 5% of max supply
 exports.TIMELOCK_DELAY = 3 * 24 * 60 * 60; // 3 days
@@ -10,16 +10,6 @@ exports.PROPOSAL_THRESHOLD = 100000; // 100K
 exports.PNG_STAKING_ALLOCATION = 500, // 5x weight in minichef
 exports.WETH_PNG_FARM_ALLOCATION = 3000, // 30x weight
 exports.INITIAL_FARMS = [
-  {
-    tokenA: "0x21cf0eB2E3Ab483a67C900b27dA8F34185991982", // WAVAX
-    tokenB: WONE,
-    weight: 2000
-  },
-  {
-    tokenA: "0xf1db872E6454D553686b088c1Ea3889cF2FE3ABe", // OG
-    tokenB: WONE,
-    weight: 2000
-  }
 ];
 exports.VESTER_ALLOCATIONS = [
   {
