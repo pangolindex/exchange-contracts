37f07eea2db55a878ff3cb30eb33e6066aafbc47
Sunur Efe Vural <efe@efe.kim>
2022-06-22 09:56:31 -0400

    import interface instead of full contract

--- contracts/staking-positions/RewardFunding.sol
+++ contracts/staking-positions/RewardFunding.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title Reward Funding
