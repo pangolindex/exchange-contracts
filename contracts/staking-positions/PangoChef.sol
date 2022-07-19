d76d5c04a9a08d02bb72b13080b39fb0a0519ce8
Sunur Efe Vural <efe@efe.kim>
2022-07-18 21:49:25 -0400

    don't use magic value

--- contracts/staking-positions/PangoChef.sol
+++ contracts/staking-positions/PangoChef.sol
@@ -131,6 +131,9 @@ contract PangoChef is PangoChefFunding, ReentrancyGuard {
     /** @notice The fixed denominator used for storing summations. */
     uint256 private constant PRECISION = 2**128;
 
+    /** @notice The initial weight of pool zero, hence the initial total weight. */
+    uint256 private constant INITIAL_WEIGHT = 1_000;
+
     /** @notice The event emitted when withdrawing or harvesting from a position. */
     event Withdrawn(
         uint256 indexed positionId,
@@ -177,8 +180,8 @@ contract PangoChef is PangoChefFunding, ReentrancyGuard {
         pools[0].rewardPair = newWrappedNativeToken;
 
         // Give 10x (arbitrary scale) weight to pool. totalWeight must never be zero from now on.
-        poolRewardInfos[0].weight = 1000;
-        totalWeight = 1000;
+        poolRewardInfos[0].weight = INITIAL_WEIGHT;
+        totalWeight = INITIAL_WEIGHT;
 
         // Initialize the immutable state variables.
         factory = newFactory;
