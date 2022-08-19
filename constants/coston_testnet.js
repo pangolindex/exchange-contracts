7bd46a337380cd7da9ca5f91d7eda1c717a47857
Sunur Efe Vural <efe@efe.kim>
2022-08-19 17:57:11 -0400

    update coston deploy parameters

--- constants/coston_testnet.js
+++ constants/coston_testnet.js
@@ -1,13 +1,13 @@
-const WCFLR = "0x3Ee7094DADda15810F191DD6AcF7E4FFa37571e4";
+const WCFLR = "0x1659941d425224408c5679eeef606666c7991a8A";
 exports.WRAPPED_NATIVE_TOKEN = WCFLR;
 exports.PNG_SYMBOL = "PCT";
 exports.PNG_NAME = "Pangolin Coston";
 exports.TOTAL_SUPPLY = 230000000; // 230M
-exports.AIRDROP_AMOUNT = 11500000; // 11.5M or 5% of max supply
+exports.AIRDROP_AMOUNT = 3450000; // 3.45M or 1.5% of max supply
 exports.TIMELOCK_DELAY = 3 * 24 * 60 * 60; // 3 days
 exports.USE_GNOSIS_SAFE = false;
 exports.PROPOSAL_THRESHOLD = 100000; // 100K
-exports.PNG_STAKING_ALLOCATION = 500, // 5x weight in minichef
+exports.PNG_STAKING_ALLOCATION = 0, // 0 weight in minichef
 exports.WETH_PNG_FARM_ALLOCATION = 3000, // 30x weight
 exports.INITIAL_FARMS = [
 ];
