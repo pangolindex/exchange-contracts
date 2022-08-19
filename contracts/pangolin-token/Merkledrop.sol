a93a2261c8c40d4572f7668df761455f359c472d
Sunur Efe Vural <efe@efe.kim>
2022-08-19 11:37:40 -0400

    update merkledrop contract

--- contracts/pangolin-token/Merkledrop.sol
+++ contracts/pangolin-token/Merkledrop.sol
@@ -1,30 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "../Claimable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Merkledrop is Claimable, Pausable {
-    mapping(address => uint256) public claimedAmounts;
+contract Merkledrop is Ownable, Pausable {
+    mapping(address => uint96) public claimedAmounts;
     IERC20 public immutable PNG;
     bytes32 public merkleRoot;
 
-    event Claimed(address account, uint256 amount);
+    event Claimed(address account, uint96 amount);
     event MerkleRootSet(bytes32 newMerkleRoot);
 
-    constructor(address airdropToken, address initialOwner) Claimable(initialOwner) {
-        require(airdropToken != address(0), "invalid token address");
+    constructor(address airdropToken, address initialOwner) {
+        require(airdropToken.code.length != 0, "invalid token address");
+        require(initialOwner != address(0), "invalid initial owner");
+        _transferOwnership(initialOwner);
         PNG = IERC20(airdropToken);
         _pause();
     }
 
-    function claim(uint256 amount, bytes32[] calldata merkleProof) external whenNotPaused {
-        require(amount != 0 && amount <= type(uint96).max, "invalid amount");
-        uint256 previouslyClaimed = claimedAmounts[msg.sender];
+    function claim(uint96 amount, bytes32[] calldata merkleProof) external whenNotPaused {
+        uint96 previouslyClaimed = claimedAmounts[msg.sender];
         require(previouslyClaimed < amount, "nothing to claim");
-        bytes32 node = bytes32(abi.encodePacked(msg.sender, uint96(amount)));
+        bytes32 node = bytes32(abi.encodePacked(msg.sender, amount));
         require(MerkleProof.verify(merkleProof, merkleRoot, node), "invalid proof");
         claimedAmounts[msg.sender] = amount;
         unchecked {
