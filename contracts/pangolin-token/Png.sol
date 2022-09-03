5048b0967966abad357071365688b021c0846c0e
Sunur Efe Vural <efe@efe.kim>
2022-09-02 22:19:53 -0400

    add voting

--- contracts/pangolin-token/Png.sol
+++ contracts/pangolin-token/Png.sol
@@ -2,17 +2,19 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Png is ERC20, ERC20Burnable, ERC20Capped, ERC20Permit, Ownable {
+contract Png is ERC20, ERC20Burnable, ERC20Permit, ERC20VotesComp, Ownable {
+    uint256 public immutable cap;
+
     constructor(uint96 cap_, uint96 preMint, string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
-        ERC20Capped(cap_)
         ERC20Permit(name_)
     {
+        cap = cap_;
         _mint(msg.sender, preMint);
     }
 
@@ -20,7 +22,22 @@ contract Png is ERC20, ERC20Burnable, ERC20Capped, ERC20Permit, Ownable {
         _mint(to, amount);
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Capped) {
+    function _maxSupply() internal view override returns (uint224) {
+        return uint224(cap);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
     }
 }
