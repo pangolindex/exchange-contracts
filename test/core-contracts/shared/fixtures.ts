6c9d8aa70574ae699ef2c0cbfedb72f8bc959c5e
Sergio <sergio2098crypto@gmail.com>
2023-01-30 21:48:43 +0300

    feat: adding tests for core contracts

---
+++ test/core-contracts/shared/fixtures.ts
@@ -0,0 +1,69 @@
+import { Contract, Wallet, ethers } from "ethers";
+import { deployContract } from "ethereum-waffle";
+
+import { expandTo18Decimals } from "./utilities";
+
+import ERC20 from "../../../build/ERC20.json";
+import PangolinFactory from "../../../build/PangolinFactory.json";
+import PangolinPair from "../../../build/PangolinPair.json";
+
+interface FactoryFixture {
+  factory: Contract;
+}
+
+const overrides = {
+  gasLimit: 9999999,
+};
+
+export async function factoryFixture(
+  [wallet]: Wallet[],
+  _: ethers.providers.Web3Provider
+): Promise<FactoryFixture> {
+  const factory = await deployContract(
+    wallet,
+    PangolinFactory,
+    [wallet.address],
+    overrides
+  );
+  return { factory };
+}
+
+interface PairFixture extends FactoryFixture {
+  token0: Contract;
+  token1: Contract;
+  pair: Contract;
+}
+
+export async function pairFixture(
+  [wallet]: Wallet[],
+  provider: ethers.providers.Web3Provider
+): Promise<PairFixture> {
+  const { factory } = await factoryFixture([wallet], provider);
+
+  const tokenA = await deployContract(
+    wallet,
+    ERC20,
+    [expandTo18Decimals(10000)],
+    overrides
+  );
+  const tokenB = await deployContract(
+    wallet,
+    ERC20,
+    [expandTo18Decimals(10000)],
+    overrides
+  );
+
+  await factory.createPair(tokenA.address, tokenB.address, overrides);
+  const pairAddress = await factory.getPair(tokenA.address, tokenB.address);
+  const pair = new Contract(
+    pairAddress,
+    JSON.stringify(PangolinPair.abi),
+    provider
+  ).connect(wallet);
+
+  const token0Address = (await pair.token0()).address;
+  const token0 = tokenA.address === token0Address ? tokenA : tokenB;
+  const token1 = tokenA.address === token0Address ? tokenB : tokenA;
+
+  return { factory, token0, token1, pair };
+}
