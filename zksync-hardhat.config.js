52f4611151883194a1645cf46f2325619c9a6119
Sunur Efe Vural <efe@efe.kim>
2023-02-02 12:40:31 +0300

    add zksync hardhat settings

---
+++ zksync-hardhat.config.js
@@ -0,0 +1,44 @@
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+
+var exports = require('./hardhat.config.js');
+const soliditySettings = exports.solidity;
+
+const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require("hardhat/builtin-tasks/task-names");
+const path = require("path");
+
+const ignoreList = [
+  "MiniChefV2Zapper.sol",
+  "WAVAX.sol",
+]
+
+subtask(
+  TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+  async (_, { config }, runSuper) => {
+    const paths = await runSuper();
+
+    return paths
+      .filter(solidityFilePath => {
+        const relativePath = path.relative(config.paths.sources, solidityFilePath)
+        const fileName = path.basename(relativePath);
+        return !ignoreList.includes(fileName);
+      })
+  }
+);
+
+module.exports = {
+  zksolc: {
+    version: "1.2.2",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zksync-goerli",
+  solidity: soliditySettings,
+  networks: {
+    "zksync-goerli": {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      zksync: true,
+    },
+  },
+};
