959142b64e819f963051e04afd80144257d698fe
Sunur Efe Vural <efe@efe.kim>
2023-03-06 12:02:25 +0300

    update pragma

--- contracts/pangolinv2-periphery/libraries/NFTDescriptor.sol
+++ contracts/pangolinv2-periphery/libraries/NFTDescriptor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.7.0;
+pragma solidity >=0.7.0 <0.8.0;
 pragma abicoder v2;
 
 import '../../pangolinv2-core/interfaces/IPangolinV2Pool.sol';
